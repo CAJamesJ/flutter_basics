a459cbefa13dc1f5417dfd1c6303d447b35f9a3c
CAJamesJ <tall032123@gmail.com>
2024-06-05 16:35:32 -0400

    upto 0xff

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,8 +11,24 @@ class HomePage extends StatelessWidget {
       //it is a widget that has many features
       // Appbar, Drawer, floating action button, bottom navigation bar, snack bar
       appBar: AppBar(
-        title: const Text('Breakfast'),
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
         centerTitle: true,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+        ),
       ),
     );
   }
