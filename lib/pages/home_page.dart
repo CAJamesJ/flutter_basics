629e63ed9c03783821efb2e2d66cf364ddaca59f
CAJamesJ <tall032123@gmail.com>
2024-06-05 16:14:19 -0400

    fonts, pubspec.yaml, terminal,  Scaffold, appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    //return type of build is widget
+    return Scaffold(
+      //it is a widget that has many features
+      // Appbar, Drawer, floating action button, bottom navigation bar, snack bar
+      appBar: AppBar(
+        title: const Text('Breakfast'),
+        centerTitle: true,
+      ),
+    );
   }
 }
