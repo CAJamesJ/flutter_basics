ffdb221571e2280965d40c5f26aa094ab0506169
CAJamesJ <tall032123@gmail.com>
2024-06-06 13:38:10 -0400

    vertical divider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,25 +10,84 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       //it is a widget that has many features
       // Appbar, Drawer, floating action button, bottom navigation bar, snack bar
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: appBar(), //extract method with name appBar
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 40,
+              left: 20,
+              right: 20,
+            ),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: const Color(0xff1D1617).withOpacity(0.11),
+                    blurRadius: 40,
+                    spreadRadius: 0.0)
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+
+                contentPadding:
+                    const EdgeInsets.all(15), //reduce the field a little
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset("assets/icons/Search.svg"),
+                ),
+                suffixIcon: IntrinsicHeight(
+                  // to use Row we have to wrap it in intrinsic height
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset("assets/icons/Filter.svg"),
+                      ),
+                    ],
+                  ),
+                ),
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              color: const Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)),
-          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
-        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: Container(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)),
+        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
       ),
     );
   }
